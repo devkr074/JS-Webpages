ae26f2fb0e84e39c033ea2a3b42b4f17860d078a
Dev Kumar <devkr074@gmail.com>
2024-12-27 18:15:18 +0530

    String Slicing in JavaScript

---
+++ Webpage_15/JS/script.js
@@ -0,0 +1,10 @@
+const fullName = "Bro Code";
+let firstName = fullName.slice(0, 3);
+let lastName = fullName.slice(4, 8);
+console.log(firstName);
+console.log(lastName);
+const email = "Bro1@gmail.com";
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+console.log(username);
+console.log(extension);
