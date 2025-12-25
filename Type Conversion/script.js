74c71e96e3ab4b19d47ed4686ef3d0e0a2cc4376
Dev Kumar <devkr074@gmail.com>
2025-12-25 19:54:07 +0530

    Type Conversion

--- Type Conversion/script.js
+++ Type Conversion/script.js
@@ -1,10 +1,7 @@
-// Type Conversion: Change the datatype of a value to another (strings, numbers, booleans)
-
 let age = window.prompt("How old are you?");
 age = Number(age);
 age += 1;
 console.log(age, typeof age);
-
 let x = "pizza";
 let y = "pizza";
 let z = "pizza";
