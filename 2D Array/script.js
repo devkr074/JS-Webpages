7862f75d7ce807f781a57d94358832c879ef5dbf
Dev Kumar <devkr074@gmail.com>
2026-01-01 00:01:28 +0530

    2D Array

--- 2D Array/script.js
+++ 2D Array/script.js
@@ -1,5 +1,3 @@
-// 2D array: Multi-dimensional array that stores a matrix of data in rows and columns. Useful for games, spreadsheets, or representing images 
-
 const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 matrix[0][0] = 'X';
 matrix[0][1] = 'O';
