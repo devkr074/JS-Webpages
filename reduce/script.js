5bfe37f24da7494a3ff514b065177a0e7bb85533
Dev Kumar <devkr074@gmail.com>
2026-01-04 14:13:09 +0530

    reduce

--- reduce/script.js
+++ reduce/script.js
@@ -1,16 +1,9 @@
-// .reduce(): reduce the elements of an array to a single value    
-
-// ----------- EXAMPLE 1 -----------
-
 const prices = [5, 30, 10, 25, 15, 20];
 const total = prices.reduce(sum);
 console.log(`$${total.toFixed(2)}`);
 function sum(accumulator, element) {
     return accumulator + element;
 }
-
-// ----------- EXAMPLE 2 -----------
-
 const scores = [75, 50, 90, 80, 65, 95];
 const maximum = scores.reduce(getMax);
 const minimum = scores.reduce(getMin);
