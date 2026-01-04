4afdfb6f9dbcf894ce09cabe53aea5d67f1d42fd
Dev Kumar <devkr074@gmail.com>
2026-01-04 14:12:02 +0530

    filter

--- filter/script.js
+++ filter/script.js
@@ -1,7 +1,3 @@
-// filter(): creates a new array by filtering out elements with a callback
-
-// ----------- EXAMPLE 1 -----------
-
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 let evenNums = numbers.filter(isEven);
 let oddNums = numbers.filter(isOdd);
@@ -12,8 +8,6 @@ function isEven(element) {
 function isOdd(element) {
     return element % 2 !== 0;
 }
-// ----------- EXAMPLE 2 -----------
-
 let ages = [16, 17, 17, 18, 19, 20, 65];
 let adults = ages.filter(isAdult);
 let children = ages.filter(isChild);
@@ -24,9 +18,6 @@ function isAdult(element) {
 function isChild(element) {
     return element < 18;
 }
-
-// ----------- EXAMPLE 3 -----------
-
 const words = ['apple', 'orange', 'kiwi', 'banana', 'pomegranate', 'coconut',];
 const longWords = words.filter(getLongWords);
 const shortWords = words.filter(getShortWords);
