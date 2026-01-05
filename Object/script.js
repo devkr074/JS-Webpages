72a5983052857b31108886b20f28d30cd8eae619
Dev Kumar <devkr074@gmail.com>
2026-01-05 10:04:50 +0530

    Object

--- Object/script.js
+++ Object/script.js
@@ -1,5 +1,3 @@
-// Object: A collection of related properties and/or methods Can represent real world objects (people, products, places) object = {key:value, function()}
-
 const person1 = {
     firstName: "Spongebob",
     lastName: "Squarepants",
