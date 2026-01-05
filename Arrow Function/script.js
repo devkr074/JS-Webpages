7e91181b4a5c7d6c6f7059a2c4b01a838fa6fd43
Dev Kumar <devkr074@gmail.com>
2026-01-05 10:03:49 +0530

    Arrow Function

--- Arrow Function/script.js
+++ Arrow Function/script.js
@@ -1,22 +1,12 @@
-// Arrow Functions: A concise way to write function expressions good for simple functions that you use only once (parameters) => some code
-
-// ---------- EXAMPLE 1 ----------
-
 const hello = (name, age) => {
     console.log(`Hello ${name}`)
     console.log(`You are ${age} years old`)
 };
 hello("Bro", 25);
-
-// ---------- EXAMPLE 2 ----------
-
 setTimeout(() => {
     console.log("Hello");
     console.log("Goodbye");
 }, 3000);
-
-// ---------- EXAMPLE 3 ---------- 
-
 const numbers = [1, 2, 3, 4, 5, 6];
 const squares = numbers.map((element) => Math.pow(element, 2));
 const cubes = numbers.map((element) => Math.pow(element, 3));
