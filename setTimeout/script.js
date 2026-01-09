fa849d34dc35157ca10a450690fb588937525583
Dev Kumar <devkr074@gmail.com>
2026-01-10 00:02:49 +0530

    setTimeout

--- setTimeout/script.js
+++ setTimeout/script.js
@@ -1,20 +1,9 @@
-// setTimeout(): Function in JavaScript that allows you to schedule the execution of a function after an amount of time Times are approximate setTimeout(callback, delay);
-
-// ---------- EXAMPLE 1 ----------
-
 function hello() {
     window.alert("Hello");
 }
 setTimeout(hello, 3000);
-
-// ---------- EXAMPLE 2 ----------
-// clearTimeout() = can cancel a timeout before it triggers
-
 const timeoutId = setTimeout(() => window.alert("Hello"), 3000);
 clearTimeout(timeoutId);
-
-// ---------- EXAMPLE 3 ----------
-
 let timeoutId2;
 function showAlert() {
     window.alert("Hello");
