5ab9c06de8f7ea8fc8fcb545c489d65034b50094
Dev Kumar <devkr074@gmail.com>
2026-01-13 10:08:54 +0530

    Error Handling

--- Error Handling/script.js
+++ Error Handling/script.js
@@ -1,8 +1,3 @@
-// Error: An Object that is created to represent a problem that occurs. Occur often with user input or establishing a connection
-// try { }: Encloses code that might potentially cause an error
-// catch { }: Catch and handle any thrown Errors from try { }
-// finally { }: (Optional) Always executes. Used mostly for clean up ex. close files, close connections, release resources
-
 try {
     const dividend = Number(window.prompt("Enter a dividend: "));
     const divisor = Number(window.prompt("Enter a divisor: "));
