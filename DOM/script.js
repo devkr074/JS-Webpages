4151625682e74869c2bc2a4f200254462860962f
Dev Kumar <devkr074@gmail.com>
2026-01-13 10:11:12 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+console.log(document);
+console.dir(document);
+document.title = "My website";
+document.body.style.backgroundColor = "hsl(0, 0%, 15%)";
+const username = "";
+const welcomeMsg = document.getElementById("welcome-msg");
+welcomeMsg.textContent += username === "" ? `Guest` : username;
