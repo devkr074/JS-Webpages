076f44a19a9c072d2d5473ea6ab59f0bb065d703
Dev Kumar <devkr074@gmail.com>
2026-01-18 11:44:19 +0530

    Callback Hell

--- Callback Hell/script.js
+++ Callback Hell/script.js
@@ -1,5 +1,3 @@
-// Callback Hell: Situation in JavaScript where callbacks are nested within other callbacks to the degree where the code is difficult to read. Old pattern to handle asynchronous functions. Use Promises + async/await to avoid Callback Hell
-
 function task1(callback) {
     setTimeout(() => {
         console.log("Task 1 complete");
